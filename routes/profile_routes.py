119d0226b55562ab2bda79a490ae4b000a5b3ba9
Uriel Martinez <martinezuri20@gmail.com>
2026-08-20 13:49:15 -0300

    creacion del perfil integrado en el modelo usuario

--- routes/profile_routes.py
+++ routes/profile_routes.py
@@ -1,7 +1,14 @@
-from flask import Blueprint
+from flask import Blueprint,render_template,session,redirect,url_for,flash
+from models.user import User
 
 profile = Blueprint("profile", __name__)
 
 @profile.route("/perfil")
 def perfil():
-    return "Perfil en desarrollo"
+    user_id=session.get("user_id")#obtener los usuarios
+    if not user_id:
+        flash("Debe iniciar sesión para ver tu perfil.", "peligro")
+        return redirect(url_for("auth.login"))
+#buscar los datos del perfil del niño
+    child=User.query.get_or_404(user_id)
+    return render_template("child/profile.html", child=child)
