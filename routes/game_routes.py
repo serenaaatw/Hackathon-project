81e628a858da219e339d134904a4270adb4420b2
serena06 <serenavargas@escuelathays.edu.ar>
2026-08-21 12:02:17 -0300

    Implementación inicial de Juego 4 + tutorial

--- routes/game_routes.py
+++ routes/game_routes.py
@@ -1,4 +1,4 @@
-from flask import Blueprint, render_template, abort
+from flask import Blueprint, render_template, abort, request
 from services.exercise_service import ExerciseService
 
 game_bp = Blueprint("game", __name__, url_prefix="/juego")
@@ -35,4 +35,37 @@ def juego3(categoria_slug):
         categoria_slug=categoria_slug,
         categoria_nombre=categoria.name,
         palabras=palabras,
+    )
+
+@game_bp.route("/4/<categoria_slug>")
+def juego4(categoria_slug):
+
+    categoria, palabras = (
+        ExerciseService.obtener_categoria_con_palabras(
+            categoria_slug
+        )
+    )
+
+    if categoria is None:
+        abort(404)
+
+
+    try:
+        dificultad = int(
+            request.args.get("dificultad", 1)
+        )
+    except (TypeError, ValueError):
+        dificultad = 1
+
+    dificultad = max(
+        1,
+        min(dificultad, 3)
+    )
+
+    return render_template(
+        "games/juego4.html",
+        categoria_slug=categoria_slug,
+        categoria_nombre=categoria.name,
+        palabras=palabras,
+        dificultad=dificultad,
     )
