1bd2ebb95c9f9bad306b983794ff3e53a338e0e8
Uriel Martinez <martinezuri20@gmail.com>
2026-08-23 14:20:25 -0300

    modificacion perfil

--- routes/profile_routes.py
+++ routes/profile_routes.py
@@ -1,14 +1,14 @@
-from flask import Blueprint,render_template,session,redirect,url_for,flash
+from flask import Blueprint, render_template, session, redirect, url_for, flash
 from models.user import User
 
 profile = Blueprint("profile", __name__)
 
 @profile.route("/perfil")
 def perfil():
-    user_id=session.get("user_id")#obtener los usuarios
+    user_id = session.get("user_id")
     if not user_id:
-        flash("Debe iniciar sesión para ver tu perfil.", "peligro")
-        return redirect(url_for("auth.login"))
-#buscar los datos del perfil del niño
-    child=User.query.get_or_404(user_id)
+        flash("Debes iniciar sesión para ver tu perfil.", "warning")
+        return redirect(url_for("auth_routes.login_route"))
+
+    child = User.query.get_or_404(user_id)
     return render_template("child/profile.html", child=child)
